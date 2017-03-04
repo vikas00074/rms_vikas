e2f1c71601eb9671183137a81ee0a12d36779735
drielnox <bl4z3r@gmail.com>
2017-03-04 00:54:06 -0300

    Se movieron metodos al codebehind del Site.Master

--- Site.Master.cs
+++ Site.Master.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace RMS
 {
-    public partial class SiteMaster : System.Web.UI.MasterPage
+    public partial class SiteMaster : MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetNoStore();
+            Response.Cache.SetExpires(DateTime.Now.AddSeconds(60));
 
+            string a;
+            a = (string)(Session["Value"]);
+            lblLogUser.Text = a;
+
+            if (lblLogUser.Text == string.Empty)
+            {
+                Response.Redirect("Default.aspx");
+            }
+        }
+
+        protected void lnkLogout_Click(object sender, EventArgs e)
+        {
+            lblLogUser.Text = string.Empty;
+            Session.Abandon();
+            Session.Clear();
         }
     }
 }
