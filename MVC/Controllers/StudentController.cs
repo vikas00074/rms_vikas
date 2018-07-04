b08b989ab2e9d36cc19207f82024fe56465d0ab6
Pablo Leiros <pablo.leiros@ar.abb.com>
2018-07-03 21:13:13 -0300

    added stylecop to asp.net mvc project

---
+++ MVC/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="StudentController.cs" company="RMS">
+// Copyright (c) RMS. All rights reserved.
+// </copyright>
+
+namespace RMS.MVC.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
