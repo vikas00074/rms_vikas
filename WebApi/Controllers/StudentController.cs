63523e8a77406ae4fe1c4562efbae9704219c755
Pablo Leiros <pablo.leiros@ar.abb.com>
2018-07-03 21:10:00 -0300

    added stylecop to webapi project

---
+++ WebApi/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="StudentController.cs" company="RMS">
+// Copyright (c) RMS. All rights reserved.
+// </copyright>
+
+namespace RMS.WebApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    public class StudentController : ApiController
+    {
+    }
+}
